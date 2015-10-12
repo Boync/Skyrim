b38f62a1a26a0f7282aa9227b7a37fd873c4f93e
boync <sempai206@gmail.com>
2015-10-12 05:11:23 +0500

    2 lab 1.0

--- Chyptography/2 lab/2 lab/Form1.Designer.cs
+++ Chyptography/2 lab/2 lab/Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_filepath = new System.Windows.Forms.TextBox();
+            this.button_view = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_encrypt = new System.Windows.Forms.Button();
+            this.button_decrypt = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox_filepath
+            // 
+            this.textBox_filepath.Location = new System.Drawing.Point(13, 13);
+            this.textBox_filepath.Name = "textBox_filepath";
+            this.textBox_filepath.Size = new System.Drawing.Size(164, 20);
+            this.textBox_filepath.TabIndex = 0;
+            // 
+            // button_view
+            // 
+            this.button_view.Location = new System.Drawing.Point(184, 9);
+            this.button_view.Name = "button_view";
+            this.button_view.Size = new System.Drawing.Size(75, 23);
+            this.button_view.TabIndex = 1;
+            this.button_view.Text = "View";
+            this.button_view.UseVisualStyleBackColor = true;
+            this.button_view.Click += new System.EventHandler(this.button_view_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button_encrypt
+            // 
+            this.button_encrypt.Location = new System.Drawing.Point(13, 91);
+            this.button_encrypt.Name = "button_encrypt";
+            this.button_encrypt.Size = new System.Drawing.Size(75, 23);
+            this.button_encrypt.TabIndex = 3;
+            this.button_encrypt.Text = "button1";
+            this.button_encrypt.UseVisualStyleBackColor = true;
+            this.button_encrypt.Click += new System.EventHandler(this.button_encrypt_Click);
+            // 
+            // button_decrypt
+            // 
+            this.button_decrypt.Location = new System.Drawing.Point(184, 91);
+            this.button_decrypt.Name = "button_decrypt";
+            this.button_decrypt.Size = new System.Drawing.Size(75, 23);
+            this.button_decrypt.TabIndex = 4;
+            this.button_decrypt.Text = "button2";
+            this.button_decrypt.UseVisualStyleBackColor = true;
+            this.button_decrypt.Click += new System.EventHandler(this.button_decrypt_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 131);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 191);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button_decrypt);
+            this.Controls.Add(this.button_encrypt);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_view);
+            this.Controls.Add(this.textBox_filepath);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox_filepath;
+        private System.Windows.Forms.Button button_view;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_encrypt;
+        private System.Windows.Forms.Button button_decrypt;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Chyptography/2 lab/2 lab/Form1.cs
+++ Chyptography/2 lab/2 lab/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace _2_lab
 {
@@ -16,5 +17,78 @@ namespace _2_lab
         {
             InitializeComponent();
         }
+
+        private void button_view_Click(object sender, EventArgs e)
+        {
+            
+            openFileDialog1.Title = "Open file...";
+            openFileDialog1.FileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+
+                textBox_filepath.Text = openFileDialog1.FileName;
+                textBox1.Text = Convert.ToString(openFileDialog1.FileName.Length);
+            
+            }
+
+        }
+
+        public byte testEncrypt(byte sByte)
+        {
+
+            sByte = (byte)(sByte * 2);
+            return sByte;
+
+        }
+
+        public byte testDecrypt(byte sByte)
+        {
+
+            sByte = (byte)(sByte / 2);
+            return sByte;
+
+        }
+
+        private void button_encrypt_Click(object sender, EventArgs e)
+        {
+
+            byte temp;
+            FileStream localFile = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+            FileStream newFile = new FileStream(openFileDialog1.FileName + ".crypt", FileMode.Create);
+            for (long i = 0; i < localFile.Length; i++)
+            {
+                temp = Convert.ToByte(localFile.ReadByte());
+                textBox2.Text = textBox2.Text + Convert.ToString(temp);
+                temp = testEncrypt(temp);
+                textBox3.Text = textBox3.Text + Convert.ToString(temp);
+                newFile.WriteByte(Convert.ToByte(temp));
+            }
+            localFile.Close();
+            newFile.Close();
+            MessageBox.Show("OK");
+        }
+
+        private void button_decrypt_Click(object sender, EventArgs e)
+        {
+
+            byte temp;
+            FileStream localFile = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+            FileStream newFile = new FileStream("test.jpg", FileMode.Create);
+            for (long i = 0; i < localFile.Length; i++)
+            {
+                temp = Convert.ToByte(localFile.ReadByte());
+                temp = testDecrypt(temp);
+                newFile.WriteByte(Convert.ToByte(temp));
+            }
+            localFile.Close();
+            newFile.Close();
+            MessageBox.Show("OK");
+        }
+
+
+
+
+
+
     }
 }
